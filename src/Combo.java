ac8170c4d449206d7099b365fb5ab25e736adbcc
Gabiru-cpu <gabriel_librande@hotmail.com>
2023-04-18 23:28:58 -0300

    Facade ENG3

---
+++ src/Combo.java
@@ -0,0 +1,12 @@
+public class Combo {
+
+    private Burguer burguer = new Burger();
+    private Sobremesa sobremesa = new Sobremesa();
+    private Bebida bebida = new Bebida();
+    private Produto produto = new Produto();
+
+    public void CriarCombo() {
+
+    }
+
+}
